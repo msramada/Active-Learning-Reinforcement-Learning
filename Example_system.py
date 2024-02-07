82e59b1fc975bcd873aa8cefe810a24b2b93ed43
Mohammad Ramadan <mramadan@moonshot.cels.anl.gov>
2024-02-07 15:38:45 -0600

    Cleaning bugs and confusions

--- Example_system.py
+++ Example_system.py
@@ -10,7 +10,7 @@ ru = 1
 ry = 1
 
 # Noise covariance, according to Anderson & Moore Optimal Filtering
-Q = torch.diag(torch.tensor([0.2, 0.2, 0.2]))
+Q = torch.diag(torch.tensor([0.5, 0.5, 0.5]))
 R = torch.tensor([[0.2]])
 A = torch.tensor([[0.92, 0.2, -0.1],[0, 0.95, -0.3],[0, 0, 0.93]])
 B = torch.tensor([[0],[0],[1.0]])
@@ -28,7 +28,9 @@ def measurementDynamics(x):
     gx = torch.zeros(ry,)
     helper = 5 * torch.tanh(torch.tensor([0.5 * (x[0]+x[1]-2.0)]))
     helper = 1/27 * (x[0]+x[1]) ** 3
-    helper = 1/27 * x[0] * x[1] * x[2]
+    a = torch.nn.ELU()
+    helper = 1/27 * a((x[0]) ** 3)# works perfectly fine
+    #helper = 1/27 * (x[0] + x[1]) ** 3 * x[0]
     gx = 0*(x[0]+x[1]+x[2]) + 1*helper
     return torch.atleast_2d(gx.squeeze()).T
 
